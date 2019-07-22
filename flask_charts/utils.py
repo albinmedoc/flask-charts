38cf8ba1cc198763c75c96fa2722bfbac857a49e
albinmedoc <stamline203@gmail.com>
2019-07-22 19:38:58 +0200

    Made so datetimes is correctly transformed to js values

--- flask_charts/utils.py
+++ flask_charts/utils.py
@@ -10,13 +10,14 @@ def prep_data(data):
     for row in data['rows']:
         for val in row['c']:
             if isinstance(val['v'], datetime.datetime):
-                val['v'] = "Date({}, {}, {})".format(val['v'].year,
-                                                     val['v'].month-1,  # JS Dates are 0-based
-                                                     val['v'].day,
-                                                     val['v'].hour,
-                                                     val['v'].minute,
-                                                     val['v'].second,
-                                                     val['v'].microsecond)
+                val['v'] = "Date({}, {}, {}, {}, {}, {}, {})".format(
+                                                    val['v'].year,
+                                                    val['v'].month-1,  # JS Dates are 0-based
+                                                    val['v'].day,
+                                                    val['v'].hour,
+                                                    val['v'].minute,
+                                                    val['v'].second,
+                                                    val['v'].microsecond)
             elif isinstance(val['v'], datetime.date):
                 val['v'] = "Date({}, {}, {})".format(val['v'].year,
                                                      val['v'].month-1,  # JS Dates are 0-based
