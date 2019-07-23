6ed256c0daf52688cc2ac804b045a90411945407
albinmedoc <stamline203@gmail.com>
2019-07-23 11:08:54 +0200

    Created a basic unittest script

---
+++ test_charts.py
@@ -0,0 +1,66 @@
+from __future__ import with_statement
+
+import json
+import sys
+
+from flask import Flask
+from flask_charts import GoogleCharts, GenericChart, BarChart, LineChart, PieChart
+
+if sys.version_info < (2, 7):
+    import unittest2 as unittest
+else:
+    import unittest
+
+
+class TestGoogleCharts(unittest.TestCase):
+    def setUp(self):
+        app = Flask(__name__)
+        app.debug = True
+        self.charts = GoogleCharts(app)
+        self.app = app
+        self.client = app.test_client()
+    
+    def tearDown(self):
+        self.app = None
+
+    def test(self):
+        self.assertTrue("renderCharts()" in self.client.get("/init_charts.js").data.decode("utf8"))
+
+    def test_init_app(self):
+        with self.assertRaises(TypeError):
+            self.charts.init_app(1)
+
+
+class TestGenericCharts(unittest.TestCase):
+    def setUp(self):
+        app = Flask(__name__)
+        app.debug = True
+        self.charts = GoogleCharts(app)
+        self.app = app
+        self.client = app.test_client()
+
+    def tearDown(self):
+        self.app = None
+
+    def test_addColumn(self):
+        chart = GenericChart("test", "PieChart")
+        chart.add_column("string", "col")
+        assert chart._columns[0][0] == "string" and chart._columns[0][1] == "col"
+        with self.assertRaises(ValueError):
+            chart.add_column("", "")
+        with self.assertRaises(TypeError):
+            chart.add_column(1)
+
+    def test_init(self):
+        with self.assertRaises(ValueError):
+            chart = GenericChart("", "PieChart")
+        with self.assertRaises(ValueError):
+            GenericChart("A B", "PieChart")
+        with self.assertRaises(ValueError):
+            GenericChart("3", "PieChart")
+        with self.assertRaises(TypeError):
+            chart = GenericChart(1)
+        #Add tests for chart type
+
+if __name__ == "__main__":
+    unittest.main()
