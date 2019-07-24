702ae106a6544aff969cd249b418c241be875751
albinmedoc <stamline203@gmail.com>
2019-07-24 15:29:45 +0200

    Addded tests for checking if ChartData is True or False

--- test_charts.py
+++ test_charts.py
@@ -4,7 +4,7 @@ import json
 import sys
 
 from flask import Flask
-from flask_charts import GoogleCharts, GenericChart
+from flask_charts import GoogleCharts, GenericChart, ChartData
 
 if sys.version_info < (2, 7):
     import unittest2 as unittest
@@ -26,6 +26,7 @@ class TestGoogleCharts(unittest.TestCase):
     def test(self):
         self.assertTrue("renderCharts()" in self.client.get("/init_charts.js").data.decode("utf8"))
 
+
     def test_init_app(self):
         with self.assertRaises(TypeError):
             self.charts.init_app(1)
@@ -41,22 +42,6 @@ class TestGenericChart(unittest.TestCase):
 
     def tearDown(self):
         self.app = None
-
-    def test_addColumn(self):
-        chart = GenericChart("PieChart", "test")
-        chart.data.add_column("string", "col")
-        assert chart.data._columns[0][0] == "string" and chart.data._columns[0][1] == "col"
-        with self.assertRaises(ValueError):
-            chart.data.add_column("", "")
-        with self.assertRaises(TypeError):
-            chart.data.add_column(1)
-    
-    def test_addRow(self):
-        chart = GenericChart("PieChart", "test")
-        chart.data.add_row(["test", 200])
-        assert chart.data._rows[0][0] == "test" and chart.data._rows[0][1] == 200
-        with self.assertRaises(TypeError):
-            chart.data.add_row("row")
 
     def test_init(self):
         with self.assertRaises(ValueError):
@@ -78,6 +63,31 @@ class TestGenericChart(unittest.TestCase):
         chart = GenericChart("PieChart", "test")
         chart.add_event_listener("ready", "my_function")
         assert chart.event_listeners[0]["event"] == "ready" and chart.event_listeners[0]["function"] == "my_function"
+
+class TestChartData(unittest.TestCase):
+    def test_addColumn(self):
+        data = ChartData()
+        data.add_column("string", "col")
+        assert data._columns[0][0] == "string" and data._columns[0][1] == "col"
+        with self.assertRaises(ValueError):
+            data.add_column("", "")
+        with self.assertRaises(TypeError):
+            data.add_column(1)
+    
+    def test_addRow(self):
+        data = ChartData()
+        data.add_row(["test", 200])
+        assert data._rows[0][0] == "test" and data._rows[0][1] == 200
+        with self.assertRaises(TypeError):
+            data.add_row("row")
+    
+    def test_bool(self):
+        data = ChartData()
+        self.assertFalse(data)
+        data.add_column("string", "col")
+        self.assertFalse(data)
+        data.add_row(["test", 200])
+        self.assertTrue(data)
 
 if __name__ == "__main__":
     unittest.main()
