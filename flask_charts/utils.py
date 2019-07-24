8309e96aae84669fcc19565801797f88f45d4bda
albinmedoc <stamline203@gmail.com>
2019-07-24 15:29:05 +0200

    Removed comments

--- flask_charts/utils.py
+++ flask_charts/utils.py
@@ -1,18 +1,12 @@
 import datetime
 
 def prep_data(data):
-    # type: (dict) -> dict
-    """Takes a dict intended to be converted to JSON for use with Google Charts and transforms date and datetime
-    into date string representations as described here:
-    https://developers.google.com/chart/interactive/docs/datesandtimes
-    TODO:  Implement Timeofday formatting"""
-
     for row in data['rows']:
         for val in row['c']:
             if isinstance(val['v'], datetime.datetime):
                 val['v'] = "Date({}, {}, {}, {}, {}, {}, {})".format(
                                                     val['v'].year,
-                                                    val['v'].month-1,  # JS Dates are 0-based
+                                                    val['v'].month-1,
                                                     val['v'].day,
                                                     val['v'].hour,
                                                     val['v'].minute,
@@ -20,13 +14,12 @@ def prep_data(data):
                                                     val['v'].microsecond)
             elif isinstance(val['v'], datetime.date):
                 val['v'] = "Date({}, {}, {})".format(val['v'].year,
-                                                     val['v'].month-1,  # JS Dates are 0-based
+                                                     val['v'].month-1,
                                                      val['v'].day)
     return data
 
 
 def render_data(columns, rows):
-    # type: (list, list) -> dict
     data = {'cols': [], 'rows': []}
 
     for column in columns:
