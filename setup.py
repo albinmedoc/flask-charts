58fec5d875eb9a620aa3a1438e1854926dda83dc
albinmedoc <stamline203@gmail.com>
2019-07-26 01:38:07 +0200

    Changed to version 1.3

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ Easily add google charts to Flask templates
 from setuptools import setup
 setup(
     name='Flask-Charts',
-    version='1.2',
+    version='1.3',
     url='https://github.com/albinmedoc/flask-charts',
     license='MIT',
     author='Albin Médoc',
