621fa2aa200a6756c168f3baa4bd4686c0517629
albinmedoc <stamline203@gmail.com>
2019-07-26 00:35:18 +0200

    Made so script can run without jQuery

--- flask_charts/static/init_charts.js
+++ flask_charts/static/init_charts.js
@@ -1,25 +1,26 @@
-$(document).ready(function () {
+window.onload = function () {
   google.charts.load('current');
   google.charts.setOnLoadCallback(renderCharts);
   function renderCharts() {
-    $('.chart').each(function (i, c) {
-      var chart_info = JSON.parse($(this).children('.chart-info').text());
+    var charts = document.getElementsByClassName("chart");
+    for (var i = 0; i < charts.length; i++) {
+      var chart_info = JSON.parse(charts[i].firstElementChild.innerHTML);
       if (chart_info.data_url) {
-        $.ajax({
-          type: "POST",
-          dataType: "json",
-          url: chart_info.data_url,
-          success: function (data) {
-            chart_info.data = data;
+        var xhr = new XMLHttpRequest();
+        xhr.onreadystatechange = function () {
+          if (xhr.readyState === 4) {
+            chart_info.data = JSON.parse(xhr.responseText);
             setup_chart(chart_info);
           }
-        });
+        };
+        xhr.open('POST', chart_info.data_url);
+        xhr.send();
       } else {
         setup_chart(chart_info);
       }
-    });
+    }
   }
-});
+}
 
 function setup_chart(chart_info) {
   temp = {
@@ -36,7 +37,7 @@ function setup_chart(chart_info) {
     if (chart_info.event_listeners) {
       chart_info.event_listeners.forEach(function (event_listener) {
         if (window[event_listener.function]) {
-          google.visualization.events.addListener(wrapper.getChart(), event_listener.event, window[event_listener.function]);
+          google.visualization.events.addListener(wrapper, event_listener.event, window[event_listener.function]);
         }
       });
     }
