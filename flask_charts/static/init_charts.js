574acc5f39f9e0be84a300730ac9667c47b27a3c
albinmedoc <stamline203@gmail.com>
2019-07-26 20:37:19 +0200

    Implemented functionality to refresh chart on given interval

--- flask_charts/static/init_charts.js
+++ flask_charts/static/init_charts.js
@@ -7,21 +7,21 @@ window.onload = function () {
     var charts = document.getElementsByClassName("chart");
     for (var i = 0; i < charts.length; i++) {
       var chart_info = JSON.parse(charts[i].firstElementChild.innerHTML);
-      if (chart_info.data_url) {
-        var xhr = new XMLHttpRequest();
-        xhr.onreadystatechange = function () {
-          if (xhr.readyState === 4) {
-            chart_info.data = JSON.parse(xhr.responseText);
-            setup_chart(chart_info);
-          }
-        };
-        xhr.open('POST', chart_info.data_url);
-        xhr.send();
-      } else {
-        setup_chart(chart_info);
-      }
+      setup_chart(chart_info);
     }
   }
+}
+
+function set_data(wrapper, data_url){
+  var xhr = new XMLHttpRequest();
+  xhr.onreadystatechange = function () {
+    if (xhr.readyState === 4) {
+      wrapper.setDataTable(JSON.parse(xhr.responseText));
+      wrapper.draw();
+    }
+  };
+  xhr.open('POST', data_url);
+  xhr.send();
 }
 
 function setup_chart(chart_info) {
@@ -29,12 +29,19 @@ function setup_chart(chart_info) {
     'containerId': chart_info.id,
     'chartType': chart_info.type,
     'dataTable': chart_info.data,
-    'dataSourceUrl': chart_info.data_url,
     'options': chart_info.options
   };
   var wrapper = new google.visualization.ChartWrapper(temp);
   google.visualization.events.addListener(wrapper, 'ready', on_ready);
   wrapper.draw();
+  if(chart_info.data_url){
+    set_data(wrapper, chart_info.data_url);
+    if(chart_info.refresh){
+      window.setInterval(function(){
+        set_data(wrapper, chart_info.data_url);
+      }, chart_info.refresh);
+    }
+  }
   function on_ready() {
     if (chart_info.event_listeners) {
       chart_info.event_listeners.forEach(function (event_listener) {
@@ -44,9 +51,6 @@ function setup_chart(chart_info) {
       });
     }
     flask_charts[chart_info.id] = {};
-    flask_charts[chart_info.id]["chart"] = wrapper.getChart();
-    if(!chart_info.data_url){
-      flask_charts[chart_info.id]["data"] = JSON.parse(chart_info.data);
-    }
+    flask_charts[chart_info.id] = wrapper.getChart();
   }
 }
